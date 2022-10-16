f9b2fa127574cfeb6279e4ee6f6f0c78841e53ce
Linus Falk <linfa084@gmail.com>
2022-10-16 23:14:39 +0200

    change back to useNavigate

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,14 +1,14 @@
 import React, { ReactNode, useEffect } from 'react'
-import { redirect } from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-
+  const navigate = useNavigate()
   const { user } = useAuth()
 
   useEffect(() => {
     if (!user?.id) {
-      redirect('/auth')
+      navigate('/auth')
     }
   }, [user?.id])
 
