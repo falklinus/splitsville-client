fed7e068e49d60885085b9441dc0bda3d3e90e75
Linus Falk <linfa084@gmail.com>
2022-10-16 22:09:00 +0200

    adding real server uri

--- src/graphql/apolloClient.tsx
+++ src/graphql/apolloClient.tsx
@@ -1,11 +1,13 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
+import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client'
+import {setContext} from '@apollo/client/link/context'
+
+const SERVER_URI = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://splitsville-server.herokuapp.com/'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001',
+  uri: SERVER_URI,
 })
 
-const authLink = setContext((_, { headers }) => {
+const authLink = setContext((_, {headers}) => {
   const token = localStorage.getItem('split_token')
   return {
     headers: {
